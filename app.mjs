0d38376df4bb82df136bbf064861e71a00f57289
Best57na <thanakrit.wtpn@gmail.com>
2026-07-30 02:39:00 +0700

    feat: implement assignments endpoint and configure database connection

--- app.mjs
+++ app.mjs
@@ -1,4 +1,7 @@
+import "dotenv/config";
 import express from "express";
+import cors from "cors";
+import pool from "./utils/db.mjs";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -15,6 +18,39 @@ app.use(express.json());
 
 app.get("/profiles", (req, res) => {
   res.status(200).json({ data: profiles.john });
+});
+
+app.post("/assignments", async (req, res) => {
+  const { title, image, category_id, description, content, status_id } =
+    req.body;
+
+  if (
+    !title ||
+    !image ||
+    !category_id ||
+    !description ||
+    !content ||
+    !status_id
+  ) {
+    return res.status(400).json({
+      message:
+        "Server could not create post because there are missing data from client",
+    });
+  }
+
+  try {
+    await pool.query(
+      `insert into assignments (title, image, category_id, description, content, status_id)
+       values ($1, $2, $3, $4, $5, $6)`,
+      [title, image, category_id, description, content, status_id]
+    );
+
+    return res.status(201).json({ message: "Created post sucessfully" });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Server could not create post because database connection",
+    });
+  }
 });
 
 app.listen(port, () => {
---
+++ utils/db.mjs
@@ -0,0 +1,12 @@
+import pg from "pg";
+
+const { Pool } = pg;
+
+const pool = new Pool({
+  connectionString: process.env.CONNECTION_STRING,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+export default pool;
